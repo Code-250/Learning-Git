b9cd45d30a539b6953b078114cb986866f013429
code-250 <richmunye@gmail.com>
2021-03-13 15:40:39 +0200

    js file

---
+++ app.js
@@ -0,0 +1,3 @@
+const taskInput = document.getElementById("new-task");
+const addButton = document.getElementById("button")[0];
+const incompleteTaskHolder = document.getElementById("incomplete-tasks");
